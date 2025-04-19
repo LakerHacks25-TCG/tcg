0401d43df1e26587938882d189058df8951b0389
Lou1337sa <louisaaf2021@gmail.com>
2025-04-19 16:33:13 -0400

    Initial commit for frontend content, this is basically nothing.

---
+++ frontend/src/routes/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    
+</script>
+
+<main> 
+
+    <p> 
+        Step: {formState.step}
+    </p>
+
+    {#if formState.step === 0}
+        <div>
+            <label for="name">Your name</label>
+
+            <input type="text" id="name" bind:value={"bind:value{formState}".name} />
+        </div>
+
+        <button 
+        onclick={() => }
+</main>
