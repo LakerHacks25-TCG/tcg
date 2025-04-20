5d5a988d429984d8e24518c4849b417ab3affc8b
Lou1337sa <louisaaf2021@gmail.com>
2025-04-19 22:00:27 -0400

    cleaned and pruned assets + basic UI framework

---
+++ frontend/LogiBeast/src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    let count = $state(0);
+    import logo from '$lib/assets/logiBeastLogo.png';
+    import bg from '$lib/assets/logiBeastBackground.png';
+</script>
+
+
+<div class="container">
+    <div class="gamebox">Game Box</div>
+    <div class="twoBoxLeft">Question box</div>
+    <div class="twoBoxRight">Answer Box</div>
+    <div class="twoBoxLeft">Tis is where the operator buttons will go and we can totally cheat by putting these in a table</div>
+    <div>c5</div>
+    <div>d5</div>
+
+
+
+
+
+
+ <!-- <div class="bg-[src={bg}]"> -->
+<!-- <div class="content">
+<h1>Welcome to SvelteKit</h1>
+
+
+{count}
+
+<button class="mathbutton" onclick={() => count++}>+</button>
+<button class="mathbutton" onclick={() => count--}>-</button>
+<button class="mathbutton" onclick={() => count++}>&#x2715;</button>
+<button class="mathbutton" onclick={() => count--}>&#247;</button>
+<br>
+<br>
+<label for="answer">?</label>
+<input type="text" id="answer" name="answer">
+
+ <img alt="LogiBeast Logo" src={logo} /> -->
+<!-- </div> --> 
+</div>
