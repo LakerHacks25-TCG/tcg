b687ae8ccb3c80a0b6f96136c1f992a5a52ca8bc
PiesArentSquare <jjhh12@outlook.com>
2025-04-19 21:37:45 -0400

    frontend proof of concept

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { backendURL } from '$lib/config';
+    import { isLoggedIn, get, getJson, post, postAndGetJson } from '$lib/netutil';
+    import { onMount } from 'svelte';
+
+    let roomName = $state('')
+    let rooms = $state([])
+    let room = $state(null)
+
+    $effect(async () => {
+        if (!await isLoggedIn()) {
+            goto('/login')
+            return
+        }
+        try {
+            rooms = await getJson('/rooms')
+        } catch (e) {
+            console.error(e)
+        }
+    })
+
+    async function create() {
+        const res = await post(`/rooms?name=${roomName}`)
+        if (res.ok)
+            goto('/room')
+        else
+            console.error('failed to create room')
+    }
+
+    async function join() {
+        const res = await post(`/rooms/${roomName}/join`)
+        if (res.ok)
+            goto('/room')
+        else if (res.status == 404)
+            console.error(`no room found with the name ${roomName}`)
+        else if (res.status == 409)
+            console.error('room full')
+        else
+            console.error('failed to join room')
+    }
+</script>
+
+<input type="text" bind:value={roomName}>
+<button onclick={create}>Create Room</button>
+<button onclick={join}>Join Room</button>
---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { goto } from '$app/navigation';
+
+    let username = $state(''), password = $state('')
+
+    async function handleLogin() {
+        const res = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({username, password}),
+            credentials: 'include'
+        })
+        if (res.ok) {
+            goto('/')
+        }
+    }
+</script>
+
+<form onsubmit={handleLogin}>
+    <input type="text" bind:value={username}>
+    <input type="password" bind:value={password}>
+    <button type="submit">Login</button>
+</form>
