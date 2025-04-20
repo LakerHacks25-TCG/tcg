a60003ac3fd15a121ad3a60de56860034916a499
PiesArentSquare <jjhh12@outlook.com>
2025-04-20 10:57:56 -0400

    asjhdb

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,5 @@
 <script>
     import { goto } from '$app/navigation';
-    import { backendURL } from '$lib/config';
-    import { isLoggedIn, get, getJson, post, postAndGetJson } from '$lib/netutil';
-    import { onMount } from 'svelte';
-    import { goto } from '$app/navigation';
 	import { backendURL } from '$lib/config';
     import { isLoggedIn, getJson, postAndGetJson, get, post } from '$lib/netutil';
 	import { Client } from '@stomp/stompjs';
@@ -62,34 +58,6 @@
     size: 85%;
 }
 </style>
-
-<div class="container">
-    <div class="hpBoxLeft"><img id="playerhp" src={hpBar} alt="playerHPBar" style="width 125%; height 125%"/></div>
-    <div class="squareBoxRight" id="enemySprite">Enemy sprite</div>
-    <div class="squareBoxLeft" id="playerSprite">Player sprite</div>
-    <div class="hpBoxRight"><img id="enemyhp" src={hpBar} alt="playerHPBar" style="width 125%; height 125%; display block; margin-left auto; margin-right auto;"/></div>
-    <div class="twoBoxLeft">Question box</div>
-    <div class="twoBoxRight">
-        <div><button class="mathbutton" onclick={() => count++}>Answer A</button></div>
-        <div><button class="mathbutton" onclick={() => count--}>Answer B</button></div>
-        <div><button class="mathbutton" onclick={() => count++}>Answer C</button></div>
-        <div><button class="mathbutton" onclick={() => count--}>Answer D</button></div>
-    </div>
-        <div class="twoBoxLeft">
-        <div><button class="mathbutton" onclick={() => count++}>+</button></div>
-        <div><button class="mathbutton" onclick={() => count--}>-</button></div>
-        <div><button class="mathbutton" onclick={() => count++}>&#x2715;</button></div>
-        <div><button class="mathbutton" onclick={() => count--}>&#247;</button></div>
-    </div>
-    <div>c5</div>
-    <div>d5</div>
-
-
-
-
-
-
-</div> 
 
 <input class="textbox-input" type="text" bind:value={roomName}>
 <button onclick={create}>Create Room</button>
